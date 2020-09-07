9cb580edc5480ea30120087aeedce545ae3479c1
Francesco Lombardi <f.lombardi79@gmail.com>
2020-09-07 11:45:58 +0200

    First commit

---
+++ database/migrations/2020_09_06_201406_create_experiencias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperienciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiencias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_trabajo');
+            $table->string('cargo');
+            $table->string('duracion');
+            $table->integer('cv_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiencias');
+    }
+}
